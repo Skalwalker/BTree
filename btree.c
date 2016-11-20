a80e58bacf0efa7cf1a7003fa38b4b1045d5e319
Renato Nobre <rekanobre@gmail.com>
2016-11-19 23:41:39 -0200

    Almost there

--- btree.c
+++ btree.c
@@ -6,64 +6,84 @@
 #define M 5
 #define TRUE 1
 #define FALSE 0
-#define T 2
+#define T 3
 
 FILE *fp;
 
+/* Structure of a key */
 typedef struct chave{
-    int ident;
-    char chave[8];
-    int prr; /* Qtd de prr, 4*/
+    int ident; /* Identifier of the key */
+    char chave[8]; /* Key as string size */
+    int prr; /* PRR Value on file */
 }t_chave;
 
+/* Structure of a node */
 typedef struct no{
-    int contador; /* Conta a contidade de chaves, 4 */
-    int folha;
-    t_chave chaves[M];//M-1 /* Estrutura de vetor do tipo t_chave, 4 */
-    struct no *pFilhos[M+1];//M /* Qtd  max de filhos 5*/
+    int contador; /* Counter */
+    int folha; /* Tell if the node is a leaf */
+    t_chave chaves[M];//M-1 /* Vector of keys */
+    struct no *pFilhos[M+1];//M /* Vector of nodes */
 }t_no;
 
-// typedef struct root{
-//     t_no *root;
-// }t_root;
+typedef struct Tree{
+    t_no *root;
+}t_tree;
 
+/* Create the node */
 t_no *cria_no(){
     int i;
+    /* Alloc memory*/
     t_no *no = (t_no*)malloc(sizeof(t_no));
     
+    /* Set counter to 0*/
     no->contador = 0;
+    /* Set the leaf indicator to false*/
     no->folha = FALSE;
+    /* Populate the vector of keys and son with default values */
     for (i=0;i<M-2;i++) {
+        /* -1 is set to a non used key */
         no->chaves[i].ident = -1;
+        /* NULL is set if the son have no pointer */
         no->pFilhos[i] = NULL;
     }
+    /* Extra NULL set because there is one more pointer to son */
     no->pFilhos[i] = NULL;
     
+    /* Return the node */
     return no;
 }
 
-t_no *criaArvore(){
-    t_no *root = (t_no*)malloc(sizeof(t_no));
-    
-    root = cria_no();
-    root->folha = TRUE;
-    
-    return root;
+/* Create a new tree */
+t_tree *criaArvore(t_tree *tree){
+    /* Create the root node */
+    t_no *x = cria_no();
+    /* Set as a leaf to true */ 
+    x->folha = TRUE;
+    /* Set the new root as the tree root */
+    tree->root = x;
+    /* Return the new root */
+    return tree;
 }
 
+/* Method for populating the key */
 t_chave *criaChave(int ident, char chave[], int estPrr){
-    t_chave *estChave = (t_chave*)malloc(sizeof(t_chave));
-    
-    estChave->ident = ident;
-    strcpy(estChave->chave, chave);
-    estChave->prr = estPrr;
-    
-    return estChave;
+    /* Alloc space for the new struct key */
+    t_chave *newKey = (t_chave*)malloc(sizeof(t_chave));
+    /* Set the stuct key identifier */
+    newKey->ident = ident;
+    /* Set the struct key key */
+    strcpy(newKey->chave, chave);
+    /* Set the struct key prr */
+    newKey->prr = estPrr;
+    /* Return the new key */
+    return newKey;
 }
 
+/* Method for handling the Command Line Arguments */
 int cliParser(int argc, char *argv[], int *registerType){
     int validate = 0;
-
+    
+    /* Check the amount of parsed args */
     if (argc != 4){
         printf("Uso: %s nome_arquivo -r 1 ou 2\n\n1 - Indica Registros de tamanho variavel\n2 - Indica Registros de tamanho fixo\n\n", argv[0]);
         
@@ -71,14 +91,17 @@ int cliParser(int argc, char *argv[], int *registerType){
     } else{
         fp = fopen(argv[1], "r" );
 
+        /* Check if the file passed as arg really exist. */
         if (fp == NULL){
             printf("\nErro ao abrir arquivo, encerrando...\n\n");
             validate = 0;
         } else {
+            /* Check if the arg is right */
             if(strcmp(argv[2],"-r")){
                 printf("\nErro! Parametro nao reconhecido, voce quis dizer '-r' ?\n\n");
                 validate = 0;
             } else {
+                /* Check if the value for the arg is valid and reference the value as an int */
                 if (strcmp(argv[3],"1") == 0){
                     printf("\nCarregando B-Tree de registros de tamanho variavel...\n\n");
                     validate = 1;
@@ -94,8 +117,12 @@ int cliParser(int argc, char *argv[], int *registerType){
             }
         }
     }
+    
+    /* Return if the CLI is ok*/
     return validate;
 }
+
+
 void splitChild(t_no *nodeX, int i, t_no *newNodeY){
     int j;
     t_no *newNodeZ = (t_no*)malloc(sizeof(t_no));
@@ -134,12 +161,13 @@ void insertNonFull(t_no *node, t_chave *toInsert){
         }
         node->chaves[i+1] = *toInsert;
         node->contador += 1;
+        
     } else {
         while(i >= 1 && ((toInsert->ident) < (node->chaves[i].ident))){
             i -= 1;
         }
         i += 1;
-        if(node->pFilhos[i]->contador == 4){
+        if(node->pFilhos[i]->contador == 2*T-1){
             splitChild(node, i, node->pFilhos[i]); // Colocar terceiro parametro;
             if((toInsert->ident) > (node->chaves[i].ident)){
                 i += 1;
@@ -150,26 +178,25 @@ void insertNonFull(t_no *node, t_chave *toInsert){
 }
 
 
-void insertBtree(t_no *root, t_chave *chave){
+void insertBtree(t_tree *tree, t_chave *chave){
     t_no *newNode;
-    t_no *rootAux;
-    
-    rootAux = root; 
-    if(root->contador == 4){
+    t_no *newRoot = tree->root;
+
+    if(newRoot->contador == 2*T-1){
         /* Caso o no esteja cheio temos que executar o split*/
         newNode = (t_no*)malloc(sizeof(t_no));
-        root = newNode;
+        tree->root = newNode;
         newNode->folha = FALSE;
         newNode->contador = 0;
-        newNode->pFilhos[1] = rootAux;
-        splitChild(newNode, 1, rootAux);
+        newNode->pFilhos[1] = newRoot;
+        splitChild(newNode, 1, newRoot);
         insertNonFull(newNode, chave);
     } else {
-        insertNonFull(rootAux, chave);
+        insertNonFull(newRoot, chave);
     }
 }
 
-void pegaChaveVariavel(t_no *root){
+void pegaChaveVariavel(t_tree *root){
     char string[8];
     int i = 0;
     int j;
@@ -191,7 +218,6 @@ void pegaChaveVariavel(t_no *root){
         chave = criaChave(k, string, i);
         insertBtree(root, chave);
         printf("\tInseriu >%d< \n", k);
-        if(k == 5) break;
         while((fgetc(fp) != '\n')&&(!feof(fp))){
             fseek(fp,i,SEEK_SET);
             i++;
@@ -199,34 +225,58 @@ void pegaChaveVariavel(t_no *root){
     }
 }
 
-void printBtree(t_no *root){
-    int i;
-    if(root == NULL){
-        return;
-    }
-    for(i = 1; i <= root->contador; i++){
-        printBtree(root->pFilhos[i]);
-        printf("%d ", root->chaves[i].ident);
-    }
-    printBtree(root->pFilhos[root->contador]);
+
+void printBtree (t_no *a, int level) {
+   int i;
+
+
+   for (i = 0; i < level; i++) { printf("  "); }
+
+
+   printf("|");
+   //printf("%d", a->contador);
+   for (i = 1; i <= a->contador; i++) {
+      printf("%d|", a->chaves[i].ident);
+
+   }
+   printf("\n");
+
+
+   for (i = 1; i <= a->contador; i++) {
+      if (a->folha == FALSE) {
+         printBtree(a->pFilhos[i], level + 1);
+      }
+   }
 }
+
+
+// void printBtree(t_no *node){
+//     int i;
+//     if(node == NULL){
+//         return;
+//     }
+//     for(i = 1; i <= node->contador; i++){
+//         printBtree(node->pFilhos[i]);
+//         printf("%d ", node->chaves[i].ident);
+//     }
+//     printBtree(node->pFilhos[node->contador]);
+// }
 
 int main(int argc, char *argv[]){
     int registerType = 0;
     int cliCheck = cliParser(argc, argv, &registerType);
-    t_no *root;
-    
-    int i = 0;
+    t_tree *root = (t_tree*)malloc(sizeof(t_tree));
 
     if (cliCheck == 1){
         if (registerType == 1){
-            root = criaArvore();
+            root = criaArvore(root);
             pegaChaveVariavel(root);
             printf("\n");
-            for(i = 1; i <= 4; i++){
-                printf("%d\n", root->chaves[i].ident);
+            int i;
+            for(i=1;i<=4;i++){
+                printf("%d ", root->root->chaves[i].ident);
             }
-            printBtree(root);
+            printBtree(root->root, 5);
         } else if(registerType == 2) {
         } else {
             printf("Erro inesperado!");
